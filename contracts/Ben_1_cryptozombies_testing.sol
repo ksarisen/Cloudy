974b6eca0a5137da1503bef9b6a904152def48dd
benjamindjukastein <bendjukastein@gmail.com>
2023-03-22 21:49:27 -0700

    added mappings for our draft 1 data structures

--- contracts/Ben_1_cryptozombies_testing.sol
+++ contracts/Ben_1_cryptozombies_testing.sol
@@ -10,6 +10,21 @@ import ".deps/ownable.sol";
  */
 contract ShardManager {
 
+    struct Shard {
+      uint shardId;
+      string shardData;
+    }
+
+    Shard[] private shards;
+    string[] private filehashes;
+    string[] private AvailableFarmerIds;
+
+    //fileHashToOwner tracks files and who owns them
+    memory mapping (string => address) private fileHashToOwner;
+    mapping (uint => string) private ShardIdtoFileHash;
+    mapping (uint => string) private ShardIdtoFarmerId;
+
+    //the following function is NOT complete. Ben will improve it
     function _storeFile(string memory _filename, address owner,uint _dna) private {
         FilesToShards.push(_filename);
     }
