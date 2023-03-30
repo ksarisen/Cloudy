864b83c352383eac26b81cf23e7df57b69421689
Jennifer Kim <75065004+jenny-km@users.noreply.github.com>
2023-03-29 18:13:31 -0700

    Update Ben_1_cryptozombies_testing.sol

--- contracts/Ben_1_cryptozombies_testing.sol
+++ contracts/Ben_1_cryptozombies_testing.sol
@@ -11,6 +11,7 @@ import ".deps/ownable.sol";
 contract ShardManager {
 
     struct Shard {
+      uint filehashId;
       uint shardId;
       string shardData;
     }
@@ -20,12 +21,66 @@ contract ShardManager {
     string[] private AvailableFarmerIds;
 
     //fileHashToOwner tracks files and who owns them
-    memory mapping (string => address) private fileHashToOwner;
+    mapping (string => address) private fileHashToOwner;
+    mapping (address => uint) private ownerFilehashCount;
     mapping (uint => string) private ShardIdtoFileHash;
+    mapping (address => uint) private shardsInFilehashCount;
     mapping (uint => string) private ShardIdtoFarmerId;
 
     //the following function is NOT complete. Ben will improve it
     function _storeFile(string memory _filename, address owner,uint _dna) private {
-        FilesToShards.push(_filename);
+        //FilesToShards.push(_filename);
     }
+
+    // [Provide all my Filehashes] return all filehashes owned by user
+    function getFilehashesByOwner(address _owner) public view returns(uint[] memory) {
+    // _owner from contract ownable
+    // we are creating a new array with the size based on the no. of filehashes the owner has
+      uint[] memory ownerFilehashes = new uint[](ownerFilehashCount[_owner]); 
+        uint counter = 0;
+        // we go through all the filehashes
+        for (uint i = 0; i < filehashes.length; i++) {
+          // if the filehash's owner is equal to the owner
+          if (fileHashToOwner[i] == _owner) {
+            // we add it to the ownerFilehashes array
+            ownerFilehashes[counter] = filehashes[i].filehashId; // not sure about this one
+            counter++;
+          }
+        }
+      return ownerFilehashes;
+    }
+
+    // [Drop Deleted Shards] Storage Provider is told to stop storing deleted data 
+    function _getShardsByFilehash (uint _filehashId) private view returns(uint[] memory) {
+      uint[] memory filehashShards = new uint[](shardsInFilehashCount[_filehashId]); 
+        uint counter = 0;
+        // we go through all the filehashes
+        for (uint i = 0; i < shards.length; i++) {
+          // if the filehash's owner is equal to the owner
+          if (shards[i].filehashId == _filehashId) {
+            // we add it to the ownerFilehashes array
+            filehashShards[counter] = shards[i].shardId; 
+            counter++;
+          }
+        }
+      return filehashShards;
+    }
+
+
+    function dropDeletedShards (uint _filehashId) public {
+      // find shards by filehashId
+      uint[] memory filehashShards = _getShardsByFilehash (_filehashId);
+      // go through all the filehash's shards
+      for (uint i = 0; i < filehashShards.length; i++) {
+        // check which farmer has the shard
+         for (uint j = 0; j < AvailableFarmerIds.length; j++) {
+           if (ShardIdtoFarmerId[i] == AvailableFarmerIds[j]) {
+              // notify farmer to drop
+              emit deleteShard(ShardIdtoFarmerId[i], )
+           }
+         }
+        }
+
+    }
+
 }
