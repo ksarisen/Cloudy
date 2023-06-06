8a731fb1ab5a6d1cb63affcd7a389d7d85a42a9e
Benjamin Djukastein <bendjukastein@gmail.com>
2023-06-05 18:48:33 -0700

    Upload, download, and delete shards endpoints are functional at a basic level but lacking logical checks

--- Cloudy-Storage-Provider/CloudyMain.py
+++ Cloudy-Storage-Provider/CloudyMain.py
@@ -1,5 +1,6 @@
-#Benjamin Djukastein
-from flask import Flask, request, render_template
+#Benjamin Djukastein, created in part via ChatGPT prompts
+import os
+from flask import Flask, abort, make_response, request, render_template, send_file
 from werkzeug.utils import secure_filename
 from dotenv import load_dotenv
 
@@ -11,28 +12,55 @@ app = Flask(__name__)
 app.debug = True
 
 @app.route('/upload', methods=['POST'])
-def upload_file():
-    # Check if the 'file' key exists in the request
-    if 'file' not in request.files:
-        return 'No file found in the request'
+def upload_shards():
+    # Check if the 'shards' key exists in the request
+    if 'shards' not in request.files:
+        return 'No shards found in the request. Try POSTING again with your desired shard files sent under the key "shards".'
 
-    file = request.files['file']
-    
-    # Check if a file was selected
-    if file.filename == '':
-        return 'No file selected'
-    
-    # Save the file to the specified path
-    filename = secure_filename(file.filename)
-    file.save(f"{os.getenv('LOCAL_STORAGE_PATH')}/{filename}")
+    shards = request.files.getlist('shards')
 
-     # Create a response with a custom message
-    response = make_response('File uploaded successfully')
-    
+    for shard in shards:
+        # Check if a file was selected
+        if shard.filename == '':
+            return 'Cannot save nameless shards.'
+        # Save the file to the specified path
+        shard_name = secure_filename(shard.filename)
+        #use the raw path from .env file to prevent backslashes from being misinterpreted.
+        shard.save(r"{}/{}".format(os.getenv('LOCAL_STORAGE_PATH'), shard_name))
+
+    # Create a response with a custom message
+    response = make_response('Shards uploaded successfully')
+
     # Set the status code to 200
     response.status_code = 200
-    
+
     return response
+
+@app.route('/download/<shardname>', methods=['GET'])
+def download_shard(shardname):
+    file_path = os.path.join(os.getenv('LOCAL_STORAGE_PATH'), shardname)
+
+    if os.path.exists(file_path):
+        return send_file(file_path, as_attachment=True)
+    else:
+        response = make_response(f"File '{shardname}' does not exist.")
+        response.status_code = 404
+        return response
+
+@app.route('/delete/<shardname>', methods=['DELETE'])
+def delete_shard(shardname):
+    file_path = os.path.join(os.getenv('LOCAL_STORAGE_PATH'), shardname)
+    if os.path.exists(file_path):
+        os.remove(file_path)
+        response = make_response(f"Shard '{shardname}' has been deleted.")
+        response.status_code = 204
+        return response
+    else:
+        abort(404, f"Shard '{shardname}' does not exist.")
+
+@app.errorhandler(404)
+def not_found_error(error):
+    return f"Error {error.code}: {error.description}", error.code
 
 @app.route('/', methods=['GET'])
 def home():
--- Cloudy-Storage-Provider/README.txt
+++ Cloudy-Storage-Provider/README.txt
@@ -5,13 +5,24 @@ Startup guide for local development:
 
 Installation guide:
 make a folder where you want to code, and clone this repo there. Also, make a folder where you will want to store the incoming files.
+Create a file called ".env" containing the line LOCAL_STORAGE_PATH = "D:/path/to/where/you/want/to/store/files" with the actual path where you want to store the uploaded files.
+Create a virtual environment using "python -m venv venv"
+Enter the virtual environment using ".\venv\Scripts\activate"
 python -m pip install numpy
 python -m pip install flask
-python -m pip install werkzeug.utils
-Create a file called ".env" containing the line LOCAL_STORAGE_PATH = "D:/path/to/where/you/want/to/store/files" with the actual path where you want to store the uploaded files.
+python -m pip install werkzeug
+python -m pip install --upgrade setuptools
+python -m pip install python-dotenv
+
 
 Run the program locally for testing using the command:
 python CloudyMain.py
 
-Interact with the running service by hitting the following endpoint with a POST containing thee file you want to upload
+Interact with the running service by hitting the following endpoint with a POST containing the file you want to upload
 http://127.0.0.1:5000/upload
+
+
+TODOS:
+Ensure the max storage limit is tracked and enforced.
+connect to the blockchain to track available shards
+Ensure only actual owner can delete relevant shards via a view external check the Provider can hit in the storage contract.
