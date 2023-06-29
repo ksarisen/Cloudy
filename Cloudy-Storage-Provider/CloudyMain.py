d75f69aa5c66425828dda903ac476dad43cf4f12
Benjamin Djukastein <bendjukastein@gmail.com>
2023-06-28 20:10:27 -0700

    added todos

--- Cloudy-Storage-Provider/CloudyMain.py
+++ Cloudy-Storage-Provider/CloudyMain.py
@@ -16,7 +16,21 @@ def upload_shards():
     # Check if the 'shards' key exists in the request
     if 'shards' not in request.files:
         return 'No shards found in the request. Try POSTING again with your desired shard files sent under the key "shards".'
+    
+    # Confirm tadding this shard will not exceed storage space
+    # Get the limit from the environment variable
+    # limit = int(os.getenv('DIRECTORY_LIMIT_BYTES'))
 
+    # # Check the usage of the directory
+    # directory = '/path/to/directory'
+    # usage = get_directory_usage(directory)
+
+    # if usage <= limit:
+    #     print("Directory usage is within the limit.")
+    # else:
+    #     print("Directory usage exceeds the limit.")
+
+    
     shards = request.files.getlist('shards')
 
     for shard in shards:
@@ -46,7 +60,9 @@ def download_shard(shardname):
         response = make_response(f"File '{shardname}' does not exist.")
         response.status_code = 404
         return response
-
+#TODO declare your storage provider info to blockchain
+#TODO: making sure file size limit is respected. Auditor method checking who is requesting the file. Updating the blockchain contract with which shards are being stored.
+#TODO: have a function the auditor can hit, which this service responds whether it has the requested file, via merkle tree ideally.
 @app.route('/delete/<shardname>', methods=['DELETE'])
 def delete_shard(shardname):
     file_path = os.path.join(os.getenv('LOCAL_STORAGE_PATH'), shardname)
@@ -65,6 +81,14 @@ def not_found_error(error):
 @app.route('/', methods=['GET'])
 def home():
     return render_template('index.html')
+
+def get_directory_usage(directory):
+    stat = os.statvfs(directory)
+    block_size = stat.f_frsize
+    total_blocks = stat.f_blocks
+    free_blocks = stat.f_bfree
+    available_space = block_size * free_blocks
+    return available_space
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
